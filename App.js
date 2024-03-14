36a26b90e6012ecd3a1ba0fc4a4d0ef157b5db85
bobakitty08 <saanvirao816@gmail.com>
2024-03-13 23:13:59 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import MentalHealth from './components/MentalHealth';
+import Productivity from './components/Productivity';
+import Money from './components/Money';
+import People from './components/People';
+
+function App() {
+  return (
+    <div className="App">
+      <Body />
+      <MentalHealth />
+      <Productivity />
+      <Money />
+      <People />
+    </div>
+  );
+}
